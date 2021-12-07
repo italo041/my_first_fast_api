d373bbf1da30375684fd04e425986f0a7a5e988f
italo041 <italo_fernando11@hotmail.com>
2021-12-06 20:50:24 -0500

    validation_body

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ app = FastAPI()
 
 #Models
 
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -63,4 +68,20 @@ def show_person_detail(
 ):
     return { person_id: 'It exists!' }
 
+# Validaciones: Request Body
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        title="Person Id",
+        description="This is the person Id",
+        gt=0
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(location.dict())
+    return results
 
